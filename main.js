7a35a2f62da91216d775d6e9161f90916ca650b7
Naomi2512 <naomichege2512@gmail.com>
2024-05-18 01:10:21 +0300

    A Project Management dashboard

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ let myButton = document.querySelector('.create')
 let Error = document.querySelector('.inputError') 
 let element5 = document.querySelector('.projectLists')
 let deleteButton = document.querySelector('.deleteButton')
+let viewProject = document.querySelector('.viewProject')
+let viewProjects = document.querySelector('.viewProjects')
+
 
 myArray = [];
+let buttonClicked = false;
+let buttonClick;
 
 myButton.addEventListener('click', () =>{
     myProject = projectName.value.trim ();
@@ -35,34 +40,82 @@ function myLists(){
         product.remove()
     })
 
-    anotherArray.forEach((element, index) => {
-        console.log (element);
-
-        element1 = element.holder;
-        console.log(element1);
-
-        let element2 = document.createElement('div');
-        element2.className = "inputDiv"
-
-        let element3 = document.createElement('p');
-        element3.textContent = element1;
-
-        let element4 = document.createElement('button');
-        element4.className = 'deleteButton'
-        element4.textContent = 'Delete'
-
-        element2.appendChild(element3)
-        element2.appendChild(element4)
-        element5.appendChild(element2)
-
-        element4.addEventListener('click', ()=>{
-            let position = index;
-            if (position !== '') {
-                myArray.splice(position , 1);
-                localStorage.setItem('db', JSON.stringify(myArray))
-            }
-            myLists();
+    if (buttonClick) {
+        anotherArray.forEach((element, index) => {
+            console.log (element);
+    
+            element1 = element.holder;
+            console.log(element1);
+    
+            let element2 = document.createElement('div');
+            element2.className = "inputDiv"
+    
+            let element3 = document.createElement('p');
+            element3.textContent = element1;
+    
+            let element4 = document.createElement('button');
+            element4.className = 'deleteButton'
+            element4.textContent = 'Delete'
+    
+            element2.appendChild(element3)
+            element2.appendChild(element4)
+            element5.appendChild(element2)
+    
+            element4.addEventListener('click', ()=>{
+                let position = index;
+                if (position !== '') {
+                    myArray.splice(position , 1);
+                    localStorage.setItem('db', JSON.stringify(myArray))
+                }
+                myLists();
+            })
+    
         })
-
+    }
+    else if (buttonClicked) {
+        anotherArray.forEach((element, index) => {
+            console.log (element);
+    
+            element1 = element.holder;
+            
+            if (element1 == projectName.value) {
+                
+            let element2 = document.createElement('div');
+            element2.className = "inputDiv"
+    
+            let element3 = document.createElement('p');
+            element3.textContent = element1;
+    
+            let element4 = document.createElement('button');
+            element4.className = 'deleteButton'
+            element4.textContent = 'Delete'
+    
+            element2.appendChild(element3)
+            element2.appendChild(element4)
+            element5.appendChild(element2)
+    
+            element4.addEventListener('click', ()=>{
+                let position = index;
+                if (position !== '') {
+                    myArray.splice(position , 1);
+                    localStorage.setItem('db', JSON.stringify(myArray))
+                }
+                myLists();
+            })
+        }
+    
     })
 }
+}
+
+
+viewProject.addEventListener('click',()=>{
+    buttonClicked = true;
+    myLists();  
+})
+
+viewProjects.addEventListener('click', ()=>{
+    buttonClick = true;
+    myLists();
+    buttonClick = false;
+})
